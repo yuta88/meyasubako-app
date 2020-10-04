f826170cb56f785de0b042fab41287c552a0cb70
yuta88 <ythys.dev@gmail.com>
2020-10-04 09:07:08 +0000

    create feed-content model & feed-contents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_085751) do
+ActiveRecord::Schema.define(version: 2020_10_04_090421) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "problem_id"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "feed_contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "content_id"
+    t.string "content_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
